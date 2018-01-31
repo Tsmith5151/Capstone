df279f322ac40ba1b45f38a6d094b24771ef6b4f
Kaitlin Kirasich <kkirasich@gmail.com>
2018-01-30 19:37:08 -0800

    Titanic log reg

--- Analysis/TitanicLogisticRegressionKK.R
+++ Analysis/TitanicLogisticRegressionKK.R
@@ -54,17 +54,17 @@ ggplot(subtrain, aes(x=Fare, y=Survived)) + geom_point() +
   stat_smooth(method="glm", se=FALSE)
 # Survival rate increases drastically as fare increases
 
-# Try model with Pclass and Fare
-model2 = glm(Survived ~ . - Pclass - Fare, data = subtrain, family = binomial)
-summary(model2)
-
-# Predict
-predict2 <- predict(model2, type = 'response')
-
-# Confusion matrix
-table(subtrain$Survived, predict2 > 0.5)
-
-#ROCR curve
-ROCRpred <- prediction(predict, subtrain$Survived)
-ROCRperf <- performance(ROCRpred, 'tpr','fpr')
-plot(ROCRperf, colorize = TRUE, text.adj = c(-0.2,1.7))
+# # Try model with Pclass and Fare
+# model2 = glm(Survived ~ . - Pclass - Fare, data = subtrain, family = binomial)
+# summary(model2)
+# 
+# # Predict
+# predict2 <- predict(model2, type = 'response')
+# 
+# # Confusion matrix
+# table(subtrain$Survived, predict2 > 0.5)
+# 
+# #ROCR curve
+# ROCRpred <- prediction(predict, subtrain$Survived)
+# ROCRperf <- performance(ROCRpred, 'tpr','fpr')
+# plot(ROCRperf, colorize = TRUE, text.adj = c(-0.2,1.7))
