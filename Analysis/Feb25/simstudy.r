25d2adb650a99dbe176af509a03a0b67be13092f
Kaitlin Kirasich <kkirasich@gmail.com>
2018-02-18 19:14:14 -0800

    Progress

--- Analysis/Feb25/simstudy.r
+++ Analysis/Feb25/simstudy.r
@@ -1,16 +1,22 @@
 library(simstudy)
 library(stringr)
 
+# Extra stuff to add
+# categorical variables
+# Vectors for variances/ mean
+# Stepwise for log reg
+# Random forest
+# UI
 
-simulateData <- function (n_noise,n_explanatory){
+simulateData <- function (n_noise,n_explanatory, n_categorical){
   simformula <- ""
   if(n_noise > 0){
-    def <- defData(varname = "rn1", dist = "normal", formula = "-1;1", variance= 0.5)
+    def <- defData(varname = "rn1", dist = "normal", formula = 0, variance= 0.5)
     if(n_noise > 1) {
       for (i in 2:n_noise) {
         simvarname <- paste(c("rn",i), collapse = " ")
         simvarname <- str_replace(simvarname, fixed(" "), "")
-        def <- defDataAdd(def,varname = simvarname, dist = "normal", formula = "-1;1", variance= 0.5)
+        def <- defDataAdd(def,varname = simvarname, dist = "normal", formula = 0, variance= 0.5)
       }
     }
   }
@@ -21,7 +27,7 @@ simulateData <- function (n_noise,n_explanatory){
     for (i in 1:n_explanatory) {
       simvarname <- paste(c("EX",i), collapse = " ")
       simvarname <- str_replace(simvarname, fixed(" "), "")
-      def <- defDataAdd(def,varname = simvarname, dist = "normal", formula = "-1;1", variance= 0.5)
+      def <- defDataAdd(def,varname = simvarname, dist = "normal", formula = 0, variance= 0.5)
       weight <- 0.1 * i
       if(i > 1) {
         simformula <- paste(c(simformula," + "), collapse = " ")
@@ -31,6 +37,7 @@ simulateData <- function (n_noise,n_explanatory){
       simformula <- paste(c(simformula,simvarname), collapse = " ")
     }
     def <- defData(def, varname = "response", dist = "binary", formula = simformula, link = "logit")
+    print(simformula)
   }
   else {
     print("No explanatory variables")
@@ -40,7 +47,7 @@ simulateData <- function (n_noise,n_explanatory){
   summary(data)
   
 }
-runModel <- function(data) {
+runLogistic <- function(data) {
   model <- lm(response ~., data = data)
   summary(model)
   #anova(model)
@@ -50,5 +57,5 @@ runModel <- function(data) {
 
 
 simulateData(10,5)
-runModel(data)
+runLogistic(data)
 
