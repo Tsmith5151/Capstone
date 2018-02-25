2928943bad6e7064c43f0528406407f7c652c62e
Kaitlin Kirasich <kkirasich@gmail.com>
2018-02-25 10:24:32 -0800

    Updated function

--- Analysis/Feb25/simstudy.r
+++ Analysis/Feb25/simstudy.r
@@ -11,12 +11,12 @@ library(stringr)
 simulateData <- function (n_noise,n_explanatory, n_categorical){
   simformula <- ""
   if(n_noise > 0){
-    def <- defData(varname = "rn1", dist = "normal", formula = 0, variance= 0.5)
+    def <- defData(varname="n1", dist="normal", formula = "0", variance = .1, link = "identity")
     if(n_noise > 1) {
       for (i in 2:n_noise) {
-        simvarname <- paste(c("rn",i), collapse = " ")
+        simvarname <- paste(c("n",i), collapse = " ")
         simvarname <- str_replace(simvarname, fixed(" "), "")
-        def <- defDataAdd(def,varname = simvarname, dist = "normal", formula = 0, variance= 0.5)
+        def <- defDataAdd(def,varname = simvarname, dist="normal", formula = "0", variance = .1, link = "identity")
       }
     }
   }
@@ -25,9 +25,9 @@ simulateData <- function (n_noise,n_explanatory, n_categorical){
   }
   if(n_explanatory > 0) {
     for (i in 1:n_explanatory) {
-      simvarname <- paste(c("EX",i), collapse = " ")
+      simvarname <- paste(c("EV",i), collapse = " ")
       simvarname <- str_replace(simvarname, fixed(" "), "")
-      def <- defDataAdd(def,varname = simvarname, dist = "normal", formula = 0, variance= 0.5)
+      def <- defDataAdd(def,varname = simvarname, dist="normal", formula = "0", variance = .1, link = "identity")
       weight <- 0.1 * i
       if(i > 1) {
         simformula <- paste(c(simformula," + "), collapse = " ")
@@ -43,19 +43,21 @@ simulateData <- function (n_noise,n_explanatory, n_categorical){
     print("No explanatory variables")
   }
   
-  data <- genData(1000, def)
-  summary(data)
+  simdata <- genData(1000, def)
+  #summary(simdata)
+  return(simdata)
   
 }
 runLogistic <- function(data) {
-  model <- lm(response ~., data = data)
-  summary(model)
+  model = lm(response ~., family = "binomial", data = data)
+  step(model)
+  #summary(model)
   #anova(model)
   predict <- predict(model, type = 'response')
   table(data$response, predict >= 0.5)
 }
 
 
-simulateData(10,5)
-runLogistic(data)
+simdata <- simulateData(10,5)
+runLogistic(simdata)
 
