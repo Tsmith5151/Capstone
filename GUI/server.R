bda46fddc678e91336ddc275e6a490270b7ac122
Trace Smith <tsmith5151@gmail.com>
2018-03-24 14:52:05 -0500

    add data sim

---
+++ GUI/server.R
@@ -0,0 +1,90 @@
+library(simstudy)
+library(ggplot2)
+library(GGally)
+library(randomForest)
+library(ROCR)
+library(caTools)
+set.seed(1)
+
+sim_data <- function(n_obs,n_noise,ndist,nvar,n_ev){
+  
+  data <- c()
+  # Noise Variables
+  for (i in 1:n_noise){
+    data<- defData(data,varname=paste0('N',i), dist=ndist, formula = "0", variance = nvar, link = "identity")
+  }
+  
+  # Explanatory Variables
+  for (i in 1:n_ev){
+    data<- defData(data,varname=paste0('EV',i), dist="normal", formula = "0", variance = .1, link = "identity")
+  }
+  
+  # EV Names
+  ev <- c()
+  weights <- round(runif(n_noise+n_ev,0,1),1)
+  for (i in (1:n_ev)){
+    ev[[i]] <- (paste0(weights[[i]],'*EV',i,sample(c('-','+'),1)))
+  }
+  
+  # Build Response Variable
+  x <- (paste(unlist(ev), collapse=''))
+  
+  y_int = 0.10
+  formula <- paste0(y_int,'+',x)
+  
+  data <- defDataAdd(data,varname="y", dist="binary", formula=substring(formula,1,nchar(formula)-1), link = "logit")
+  
+  # Build Simulated Data 
+  data <- as.data.frame.matrix(genData(n_obs,data))
+  
+  return(data[,2:(ncol(data)-1)])
+}
+
+
+plot_data <- function(data){
+  
+  # Parameters:
+  # ---------------
+  # data: input simulated dataframe
+  
+  # Returns:
+  # ---------------
+  # Scatter Matrix of Simulated Dataset
+  
+  # Only plot continous variables
+  data <-data[,2:(ncol(data)-1)]
+  cols <- sapply(data,is.integer)
+  data <- data[,!cols]
+  
+  p <- ggpairs(data,lower=list(continuous=wrap("smooth", colour="navy")),
+               diag=list(continuous=wrap("barDiag", fill="darkcyan")))
+  
+  plot <- p + labs(title= paste("Simulated Dataset")) + theme(plot.title = element_text(size=14,face="bold")) + 
+    theme(plot.title = element_text(hjust = 0.5))
+  
+  return(plot)
+}
+
+
+server <- function(input, output) {
+  
+  # Return the requested dataset ----
+  simdata <- reactive({
+    sim_data(input$nrows,input$noise,input$ndist,input$nvar,input$ev)
+  })
+
+  # Show Table
+  output$table <- renderTable({
+    head(simdata(), n = 10)
+  })
+  
+  # Summary Table
+  output$summary <- renderTable({
+    summary(simdata())
+  })
+    
+  # Show Plot
+  output$plot <- renderPlot({
+    plot_data(simdata())
+  })
+}
--- GUI/ui.R
+++ GUI/ui.R
@@ -1,56 +1,53 @@
-library(shiny)
-library(shinydashboard)
-
-###############################
-# UI Code
-###############################
-
-sidebar <- dashboardSidebar(
-             sidebarMenu(
-               menuItem('Dashboard',tabName='dash',icon=icon('dashboard')),
-               menuItem('Literature',tabName='lit_review',icon=icon('book')),
-               menuItem('Data Simulation',tabName = 'data',icon=icon('table')),
-               menuItem('Models',tabName = 'model',icon=icon('random')),
-               menuItem('Visualizations',tabName = 'plots',icon=icon('area-chart'))
-             )
-           )
-
-body <- dashboardBody(
-  tabItems(
-    tabItem(tabName = "dash",
-            titlePanel("Capstone Project"),
-            HTML('<center><img src="smu.jpg" width=500 height=300></center>')
-    ),
-    tabItem(tabName = "lit_review",
-            h2("Literature Review")
-    ),
-    tabItem(tabName = "data",
-            titlePanel("Select or Simulate Dataset"),
-            fluidPage(
-              sidebarLayout(
-                # Load Data
-                sidebarPanel(width=6,
-                             fileInput("file1", "Choose CSV File",
-                                       accept = c(
-                                         "text/csv",
-                                         "text/comma-separated-values,text/plain", ".csv")), 
-                             tags$hr(),
-                             
-                             # Add checkbox if header exists
-                             checkboxInput("header", "Dataset has Header", TRUE),
-                             
-                             # Type of file Separator
-                             radioButtons("sep", "Separator",
-                                          choices = c(Comma = ",",
-                                                      Semicolon = ";",
-                                                      Tab = "\t"),
-                                          selected = ",")
-                              ),
+# Define UI for dataset viewer app ----
+ui <- fluidPage(
+  
+  # Title
+  titlePanel("SMU Data Science"),
+  
+  # Sidebar layout with a input and output definitions ----
+  sidebarLayout(
     
-                             # Main view to display results
-                             mainPanel(tableOutput('contents'))
-                )
-            )
-        )
-   )
+    # Sidebar Inputs
+    sidebarPanel(
+      
+      # Input: Number of Rows
+      numericInput(inputId = "nrows",
+                   label = "Number of Rows",
+                   value = 1,min=0,max = 100),
+      
+      br(),
+      # Input: Noise Variables
+      numericInput(inputId = "noise",
+                   label = "Noise Variables",
+                   value = 1,min=0,max = 100),
+      
+      # Distribution for Noise Variables
+      selectInput("ndist", label ="Noise Variable Distribution", 
+                  choices = list("Normal" = "normal", 
+                                 "Gamma" = "gamma"), 
+                  selected = "normal"),
+      
+      # Input: Noise Variables Variance
+      numericInput(inputId = "nvar",
+                   label = "Noise Varance",
+                   value = 1,min=0,max = 10),
+      
+      br(),
+      # Input: Explanatory Variables
+      numericInput(inputId = "ev",
+                   label = "Explanatory Variable",
+                   value = 1,min=0,max = 100)
+    ),
+    
+    # Main Panel
+    mainPanel(
+      
+      # Outputs
+      tabsetPanel(
+        tabPanel("Simulated Data", tableOutput("table")), 
+        tabPanel("Summary", tableOutput("summary")), 
+        tabPanel("Visualization", tableOutput("plot"))
+    )
+  )
+)
 )
